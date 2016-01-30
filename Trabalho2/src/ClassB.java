bfb9d9d572af07ae78d2913dd55de2d13bec1304
Breno Rios Zeymer <bzeymer@gmail.com>
2016-01-30 17:10:23 -0200

    Corrigido erro na classe B

--- Trabalho2/src/ClassB.java
+++ Trabalho2/src/ClassB.java
@@ -3,13 +3,14 @@ public class ClassB {
 	private String B4;
 	private int B1;
 	private float B2;
-	
+
 	public int getB1() {
 		return B1;
 	}
 
 	public void setB1(int b1) {
 		B1 = b1;
+	}
 
 	public String getB4() {
 		return B4;
@@ -18,13 +19,14 @@ public class ClassB {
 	public float getB2() {
 		return B2;
 
+	}
+
 	public void setB4(String b4) {
 		B4 = b4;
 	}
-	
+
 	public void setB2(int b2) {
 		B2 = b2;
 	}
-
 
 }
